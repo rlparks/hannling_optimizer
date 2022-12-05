703c8f4aef7bf8cbc7628193dd8eeb2bd87b4c54
rlparks <rebecca.parks@uga.edu>
2022-12-05 11:49:53 -0500

    Initial main

---
+++ Optimizer.cpp
@@ -0,0 +1,15 @@
+#include "Optimizer.hpp"
+#include "Hannling.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void readHannlingFile(vector<Hannling> & hannlings);
+
+int main() {
+    cout << "Hannling Optimizer\n------------------\n\n";
+    cout << "Read from file? (y/n) ";
+    char file = 0;
+    cin >> file;
+}
